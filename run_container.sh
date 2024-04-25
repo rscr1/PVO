3b0f2b7ffa422c6154dc20b29f227c8c917db4ab
rscr1 <damir3933@gmail.com>
2024-04-25 15:36:02 +0300

    fix docker commands

--- run_container.sh
+++ run_container.sh
@@ -1,4 +1,4 @@
-docker stop dar_pvo_cont_1
-docker rm -v dar_pvo_cont_1
+docker stop pvo_cont_0
+docker rm -v pvo_cont_0
 # docker run --gpus all -it --name pvo -v /home:/home --ipc=host pvo_running:latest bash
-docker run -it -Pd --gpus='"device=0,1"' --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /storage/3030/AkhmetzyanovD/projects/pvo:/pvo -v /:/root --name dar_pvo_cont_1 dar_pvo:v1 bash
+docker run -it -Pd --gpus='"device=0,1"' --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /path/to/pvo/on/local/machine:/pvo -v /:/root --name pvo_cont_0 pvo:v0
